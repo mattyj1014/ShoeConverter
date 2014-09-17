a8ea312b95ac297e8d9b9851e856fe6af3dd0180
Matthew Jenks <matthewjenks@Matthews-MacBook-Pro.local>
2014-09-17 15:22:54 -0700

    first v of project

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    
+    
+    @IBAction func convertButtonPressed(sender: UIButton)
+    {
+        
+//        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false
+        mensConvertedShoeSizeLabel.text = "\(mensShoeSizeTextField.text.toInt()! + 30)" + "in European Show Size"
+        
+    }
+    
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: UIButton)
+    {
+        let sizeFromTextField = Double ((womensShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + "in European Shoe Size"
+        
+        
+    }
+    
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
